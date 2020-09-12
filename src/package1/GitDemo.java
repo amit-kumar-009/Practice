a3471fe6e127d104e2a1fb007e9d18f1afcf803e
amit <amitalok009@gmail.com>
2020-09-12 16:51:10 +0530

    Branch creator

--- src/package1/GitDemo.java
+++ src/package1/GitDemo.java
@@ -5,6 +5,7 @@ public class GitDemo {
 	public static void main(String[] args) {
 		System.out.println("I am 1st");
 		System.out.println("I am changing it for the 1st time");
+		System.out.println("I am creating one branch for the 1st time");
 	}
 
 }
